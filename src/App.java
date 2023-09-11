0ddc9962d9b16021a3dca32ccce957950ba2b956
Noah Söng <noah.song@elev.ga.ntig.se>
2023-09-11 09:12:48 +0200

    lagt till inmatning med kvot i heltal och decimal

--- src/App.java
+++ src/App.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
+   
     int   tal1=8;
     int  tal2=6;
     double kvotD=(double)tal1/tal2;
     System.out.println("Kvoten i decimal blir"+kvotD);
     double kvotH=tal1/tal2;
-    System.out.println("Kvoteni heltal blir"+kvotH);
+    System.out.println("Kvoten i heltal blir"+kvotH);
+
+
+    Scanner tangentbord= new Scanner(System.in);
+    System.out.println("Ange täljare");
+    int tal3=tangentbord.nextInt();
+    System.out.println("Ange nämnare");
+    int tal4=tangentbord.nextInt();
+    double kvot3=(double)tal3/tal4;
+    System.out.println("Kvoten i decimal blir 4 "+kvot3);
+    double kvot4=tal1/tal2;
+    System.out.println("Kvoten i heltal blir"+kvot4);
+
+
+
     }
 }
