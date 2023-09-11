5e9200ca92ed5577355c613848bde83077decbba
Noah Söng <noah.song@elev.ga.ntig.se>
2023-09-11 09:00:03 +0200

    Gjorde dataomvandling från int till double

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    int   tal1=8;
+    int  tal2=6;
+    double kvotD=(double)tal1/tal2;
+    System.out.println("Kvoten i decimal blir"+kvotD);
+    double kvotH=tal1/tal2;
+    System.out.println("Kvoteni heltal blir"+kvotH);
+    }
+}
